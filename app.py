ab8d89711ae3fe143620e0b9934623e5d5aa6b11
John Miner <john.douglas.miner.iv@gmail.com>
2019-10-17 09:31:59 -0700

    Changed javascript

--- app.py
+++ app.py
@@ -17,6 +17,73 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 
 
+class world():
+    def __init__(self, rows, coloumns):
+        self.rows = rows
+        self.coloumns = coloumns
+        self.map_positions = []
+        self.characters = []
+
+    def world_create(self):
+        for coloumn in range(self.coloumns):
+            self.map_positions.append([])
+            for row in range(self.rows):
+                self.map_positions[coloumn]
+                spot = spots(coloumn, row)
+                self.map_positions[coloumn].append(spot)
+
+    def character_pos(self):
+        for coloumn in self.map_positions:
+            for position in coloumn:
+                position.occupied = None
+                for character in self.characters:
+                    if position.x_cord == character.x and position.y_cord == character.y:
+                        position.occupied = character.color
+
+    def return_location(self, x=7, y=7):
+        location = []
+        position_count = 0
+        check = 0
+        for coloumn in range(x-2, x+3):
+            for row in range(y-2, y+3):
+                if coloumn < self.coloumns:
+                    for position in self.map_positions[coloumn]:
+                        if position.x_cord == coloumn and position.y_cord == row:
+                            if position.occupied is not None:
+                                location.append(position.occupied)
+                                position_count += 1
+                            else:
+                                location.append(position.color)
+                                position_count += 1
+                check += 1
+                if position_count < check:
+                    position_count += 1
+                    location.append('#000000')
+
+        return location
+
+
+class spots():
+    def __init__(self, x, y):
+        self.x_cord = x
+        self.y_cord = y
+        self.color_list = ['#FE1B04', '#FE9C04', '#F6FE04', '#01FD03', '#01FDB4', '#0174FD', '#C801FD']
+        self.color = random.choice(self.color_list)
+        self.occupied = None
+
+
+class character():
+    def __init__(self):
+        self.id = str(random.random())
+        self.x = 7
+        self.y = 7
+        self.color = '#030303'
+
+
+new_world = world(20, 20)
+new_world.world_create()
+
+
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
@@ -102,70 +169,6 @@ def test_disconnect():
     print('Client disconnected', request.sid)
 
 
-class world():
-    def __init__(self, rows, coloumns):
-        self.rows = rows
-        self.coloumns = coloumns
-        self.map_positions = []
-        self.characters = []
-
-    def world_create(self):
-        for coloumn in range(self.coloumns):
-            self.map_positions.append([])
-            for row in range(self.rows):
-                self.map_positions[coloumn]
-                spot = spots(coloumn, row)
-                self.map_positions[coloumn].append(spot)
-
-    def character_pos(self):
-        for coloumn in self.map_positions:
-            for position in coloumn:
-                position.occupied = None
-                for character in self.characters:
-                    if position.x_cord == character.x and position.y_cord == character.y:
-                        position.occupied = character.color
-
-    def return_location(self, x=7, y=7):
-        location = []
-        position_count = 0
-        check = 0
-        for coloumn in range(x-2, x+3):
-            for row in range(y-2, y+3):
-                if coloumn < self.coloumns:
-                    for position in self.map_positions[coloumn]:
-                        if position.x_cord == coloumn and position.y_cord == row:
-                            if position.occupied is not None:
-                                location.append(position.occupied)
-                                position_count += 1
-                            else:
-                                location.append(position.color)
-                                position_count += 1
-                check += 1
-                if position_count < check:
-                    position_count += 1
-                    location.append('#000000')
-
-        return location
-
-
-class spots():
-    def __init__(self, x, y):
-        self.x_cord = x
-        self.y_cord = y
-        self.color_list = ['#FE1B04', '#FE9C04', '#F6FE04', '#01FD03', '#01FDB4', '#0174FD', '#C801FD']
-        self.color = random.choice(self.color_list)
-        self.occupied = None
-
-
-class character():
-    def __init__(self):
-        self.id = str(random.random())
-        self.x = 7
-        self.y = 7
-        self.color = '#030303'
-
 
 if __name__ == '__main__':
-    new_world = world(20, 20)
-    new_world.world_create()
     socketio.run(app, debug=True, host='0.0.0.0', port=8000)
