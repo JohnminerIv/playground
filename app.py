08da62a81a87fe5f6971fce1c899dee3c59b8aff
John Miner <john.douglas.miner.iv@gmail.com>
2019-10-18 10:22:32 -0700

    switch procofile again

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from threading import Lock
 from flask import Flask, render_template, session, request, \
     copy_current_request_context
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
